d092415895aa74dc777104dbb1b0b8c08667386f
HABYALIMANA KAYIRANGA <khplacide@gmail.com>
2025-07-10 13:58:48 +0000

    station_dimension model commit

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+WITH BIKE as (
+select 
+distinct
+start_statio_id as start_station_id,
+start_station_name,
+start_lat,
+start_lng
+from {{ source('demo', 'bike') }}
+where RIDE_ID != 'ride_id'
+
+)
+
+select
+*
+from BIKE
