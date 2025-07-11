447eebd846afa60ffadb2b88fa3f13d5e13f8dbd
HABYALIMANA KAYIRANGA <khplacide@gmail.com>
2025-07-11 11:31:48 +0000

    stg_bike commit model

---
+++ models/weather_bike_correlation.sql
@@ -0,0 +1,12 @@
+WITH CTE AS(
+    select 
+    t.*,
+    w.*
+
+    from {{ ref('trip_fact') }} t 
+    left join {{ ref('daily_weather') }} w 
+    on t.date_trip = w.daily_weather
+)
+
+select *
+from CTE
