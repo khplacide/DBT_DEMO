619b7cfc943dc0b9f2a0a39e99ed53083be291f2
HABYALIMANA KAYIRANGA <khplacide@gmail.com>
2025-07-14 09:33:55 +0000

    commit change models

--- models/weather_bike_correlation.sql
+++ models/weather_bike_correlation.sql
@@ -6,8 +6,8 @@ WITH CTE AS(
     from {{ ref('trip_fact') }} t 
     left join {{ ref('daily_weather') }} w 
     on t.date_trip = w.daily_weather
+    where date_trip < '2020-01-01'
 
-    order by TRIP_DATE desc
 )
 
 select *
